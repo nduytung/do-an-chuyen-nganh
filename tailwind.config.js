9d36dd083b1acc62822232778ca9f0595e499073
nduytung <nduytung.1611@gmail.com>
2022-06-03 07:35:21 +0700

    resolve conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
     extend: {},
-    screens: {
-      'iphone' : '414px',
-      'md' : '768px',
-      'lg' : '1024px',
-      'xl' : '1280px',
-    }
   },
   plugins: [],
 };
